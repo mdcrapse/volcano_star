33dd93233e59185a75a14d50f89141855ae3d4f9
Michael Crapse <mikeeybikeey@gmail.com>
2021-11-12 12:54:37 -0700

    Make HotbarSlot inherit PackSlot

--- gui/hotbar.lua
+++ gui/hotbar.lua
@@ -1,44 +1,30 @@
 local Node = require('gui.node')
+local PackSlot = require('gui.pack_slot')
 local input = input
 
 local HotbarSlot = {}
 HotbarSlot.__index = HotbarSlot
-setmetatable(HotbarSlot, {__index = Node})
+setmetatable(HotbarSlot, {__index = PackSlot})
 
 function HotbarSlot.new(player, hotbar_index)
-    local self = setmetatable(Node.new(), HotbarSlot)
-    self.w = 20
-    self.h = 20
-    self.filter_cursor = true
-    self.player = player
+    local self = setmetatable(PackSlot.new(player, player.pack, hotbar_index),
+                              HotbarSlot)
     self.hotbar_index = hotbar_index
     return self
 end
 
-function HotbarSlot:onHover(game)
-    if input:isMousePress(1) then
-        local slot = self.player.pack.slots[self.hotbar_index]
-        local cursor = self.player.cursor_slot
-        if slot:canMerge(cursor) then
-            slot:merge(cursor)
-        else
-            cursor:swap(slot)
-        end
-    end
-end
-
-function HotbarSlot:tick(dt, game) Node.tick(self, dt, game) end
-
 function HotbarSlot:draw(game)
     if self.player.cur_slot == self.hotbar_index then
-        love.graphics.draw(game.assets.sprites.hotbar_slot_selected, self.x, self.y)
+        love.graphics.draw(game.assets.sprites.hotbar_slot_selected, self.x,
+                           self.y)
     else
         love.graphics.draw(game.assets.sprites.hotbar_slot, self.x, self.y)
     end
     local slot = self.player.pack.slots[self.hotbar_index]
     if slot.item then
         love.graphics.draw(slot.item.sprite, self.x + 2, self.y + 2)
-        love.graphics.print(tostring(slot.count), self.x + 1, self.y + self.h - 6)
+        love.graphics.print(tostring(slot.count), self.x + 1,
+                            self.y + self.h - 6)
     end
 
     for i, child in pairs(self.children) do child:draw(game) end
@@ -51,6 +37,7 @@ setmetatable(Hotbar, {__index = Node})
 
 function Hotbar.new(player)
     local self = setmetatable(Node.new(), Hotbar)
+    self.player = player
 
     local NUM_SLOTS = 10
 
@@ -64,6 +51,16 @@ function Hotbar.new(player)
     end
 
     return self
+end
+
+function Hotbar:tick(dt, game)
+    for name, child in pairs(self.children) do child.pack = self.player.pack end
+    Node.tick(self, dt, game)
+end
+
+function Hotbar:draw(game)
+    for name, child in pairs(self.children) do child.pack = self.player.pack end
+    for i, child in pairs(self.children) do child:draw(game) end
 end
 
 Hotbar.Slot = HotbarSlot
