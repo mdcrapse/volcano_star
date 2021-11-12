1cf4379898d8e767bcce5a283955798cb10d6f87
Michael Crapse <mikeeybikeey@gmail.com>
2021-11-12 10:06:05 -0700

    Modify tool bench recipes

--- item/weapon_bench.lua
+++ item/weapon_bench.lua
@@ -8,31 +8,29 @@ return {
             time = 180,
             output = {item = 'iron_pickaxe', count = 1},
             input = {
-                {item = 'pickaxe', count = 1}, {item = 'iron_bar', count = 5}
+                {item = 'wood', count = 10}, {item = 'iron_bar', count = 10}
             }
         }, {
             time = 300,
             output = {item = 'gold_pickaxe', count = 1},
             input = {
-                {item = 'iron_pickaxe', count = 1},
-                {item = 'gold_bar', count = 5}
+                {item = 'wood', count = 10}, {item = 'gold_bar', count = 10}
             }
         }, {
             time = 60,
             output = {item = 'iron_shovel', count = 1},
             input = {
-                {item = 'shovel', count = 1}, {item = 'iron_bar', count = 3}
+                {item = 'wood', count = 10}, {item = 'iron_bar', count = 3}
             }
         }, {
             time = 120,
             output = {item = 'gold_shovel', count = 1},
             input = {
-                {item = 'iron_shovel', count = 1},
-                {item = 'gold_bar', count = 3}
+                {item = 'wood', count = 10}, {item = 'gold_bar', count = 3}
             }
         }, {
             time = 60,
-            output = {item = 'bomb', count = 1},
+            output = {item = 'bomb', count = 2},
             input = {
                 {item = 'wood', count = 1}, {item = 'copper_bar', count = 1}
             }
